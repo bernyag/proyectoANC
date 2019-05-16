e719ced2d8a2f5d2cfb1693365d22787ae94c2aa
Manuel Fdz <manufdzver@hotmail.com>
2019-05-16 13:21:47 -0500

    manu

--- ivps.m
+++ ivps.m
@@ -1,5 +1,5 @@
 function [t,y] = ivps(f, t0, tf, y0, h, solver)
-    
+    [m,n] = size(y0);
     switch solver
         case 1
             fhandle = @euler;
@@ -7,18 +7,25 @@ function [t,y] = ivps(f, t0, tf, y0, h, solver)
             fhandle = @trap;
         case 3
             fhandle = @midpoint;
+        case 4
+            fhandle = @ralston;
         otherwise
             fhandle = @rk4;
     end
 
     t = t0:h:tf;
     %y = zeros(size(t)); este era para solo vector ahora queremos matriz
-    y = zeros(length(y0),  length(t));
-    y(:, 1:3)=y0;
+    y = zeros(length(y0),  length(t)*n);
+    y(:, 1:n)=y0;
     
     for i = 1:length(t)-1
-        y(:,i+1) = y(:,i) + h*fhandle(f,t(i), y(:,i), h);
+        yn = y0 + h*fhandle(f,t(i), y0, h);
+        for j = (i*n)+1:(i*n)+1+n-1
+            y(:,j) = yn(:,j-(i*n));
+        end
+        y0=yn;
     end
+    
     
     function phi = euler(f, t, y, ~)
         phi = f(t,y);
@@ -38,18 +45,6 @@ function [t,y] = ivps(f, t0, tf, y0, h, solver)
     end
 
     function phi = rk4(f, t, y, h)
-%         s1 = f(t,y);
-%         yp1 = y + (h/2) * s1;
-%         
-%         s2 = f(t+h/2, yp1);
-%         yp2 = y + (h/2) * s2;
-%         
-%         s3 = f(t+h/2, yp2);
-%         yp3 = y + h * s3;
-%         
-%         s4 = f(t+h, yp3);
-%         
-%       phi = (s1 + 2*s2 + 2*s3 +s4)/6;
 
         s1 = f(t,y);     
         s2 = f(t+h/2, y + (h/2) * s1);       
@@ -57,6 +52,14 @@ function [t,y] = ivps(f, t0, tf, y0, h, solver)
         s4 = f(t+h, y + h * s3);
         
         phi = (s1 + 2*s2 + 2*s3 +s4)/6;
+    end
+
+    function phi = ralston(f, t, y, h)
+
+        s1 = f(t,y);     
+        s2 = f(t+(3/4)*h, y + (3/4) * s1 * h);       
+        
+        phi = (1/3)*s1 + (2/3)*s2;
     end
 
     
